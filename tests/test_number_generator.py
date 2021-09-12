db974f5b68c6e3f61debd3bfad04d1bd41b975a6
Pablo Carneiro Elias <pablo.cael@gmail.com>
2021-09-12 18:08:18 +0900

    Writting tests for number generator (in progress)

---
+++ tests/test_number_generator.py
@@ -0,0 +1,13 @@
+from number_generator import generate_numbers_sequence
+
+def test_output_size():
+
+    test_sequences = [
+        [1,2,3,4,5,6,1,2,3,4,5],
+        [1,2,4,5,6,2,3,4,5],
+        [0,2,4,5,6,1,2,3,4,5],
+        [0,,4,5,,1,2,3,9,1,1,4,5]
+    ]
+
+
+
