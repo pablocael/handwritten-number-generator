Added more tests and fixed a off by one bug

number_generator/core.py
@@ -255,9 +255,16 @@ class DigitSequenceImageGenerator(ImageGenerator):
         return self._generate_blank_block(space_width, height)
 
 
-    def generate_from_data(self, digits: Iterable):
+    def generate_from_data(self, digits: Iterable) -> np.ndarray:
+
+        """
+        Synthesize an image composed of handwritten digits from the input number sequence
+        """
 
         N = len(digits) # number of total digits in the sequence
+        if N == 0:
+            # if no digitis are given, returns a black strip (background only)
+            return np.ones((28, self._output_image_width), dtype=np.uint8) * 255
 
         # stores the intermediate generate sequence of images that corresponds to
         # each digit in input sequence plus spacer images
@@ -272,7 +279,6 @@ class DigitSequenceImageGenerator(ImageGenerator):
             x0, _, x1, _ = helpers.calculate_binary_image_contents_bbox(digit_image)
 
             # crop digit contents on x axis
-            # adding 1 extra pixel in x1 (x max coordinate) because it marks relevant data pixel and we need to crop background only.
             digit_image = digit_image[:,x0:x1+1]
 
             intermediate_result_images.append(digit_image)

number_generator/helpers.py
@@ -27,12 +27,17 @@ def zero_pad_centered_axis(image: np.ndarray, axis: int, new_size: int):
     diff = (new_size - current_size)
 
     pad_size =  diff // 2
-    pad_overflow = diff % 2 # if we cannot perfectly pad, put extra pixel on the right or image
+
+    # pad size is diff // 2
+    pad_left = pad_size
+
+    # pad right is the remaining pixels. this is needed because diff / 2 can have remaining of 1
+    pad_right = new_size - current_size - pad_size
 
     # pad only given axis dimension
 
     dims_pad = [(0,0), (0,0)]
-    dims_pad[axis] = (pad_size, pad_size + pad_overflow)
+    dims_pad[axis] = (pad_left, pad_right)
     return np.pad(image, pad_width=dims_pad, mode='constant')
 
 def rescale_to_width(image: np.ndarray, new_width) -> np.ndarray:
@@ -57,7 +62,10 @@ def rescale_to_width(image: np.ndarray, new_width) -> np.ndarray:
     scale_factor = new_width / original_width
 
     pil_image = Image.fromarray(image)
-    pil_image = pil_image.resize((int(pil_image.width * scale_factor), int(pil_image.height * scale_factor)), Image.LANCZOS)
+
+    scaled_width = int(np.round(pil_image.width * scale_factor))
+    secaled_height = int(np.round(pil_image.height * scale_factor))
+    pil_image = pil_image.resize((scaled_width, secaled_height), Image.LANCZOS)
 
     # now pad with zeros on y dimension to keep original height
 

tests/test_helpers.py
@@ -1,7 +1,7 @@
 from number_generator import helpers
 import numpy as np
 
-def test_helpers_calculate_binary_image_contents_bbox():
+def test_calculate_binary_image_contents_bbox():
 
     # create an empty image
     empty_image = np.zeros((28, 28), dtype=np.uint8)
@@ -31,4 +31,22 @@ def test_helpers_calculate_binary_image_contents_bbox():
 
     # bbox of empty image (background only) should be all zeros
     assert bbox == (0, 0, 99, 99)
+
+def test_zero_pad_centered_axis():
+
+    # test non divisible by two width padding
+    output_width = 111
+    input_width = 50
+    input_height = 28
+
+    input_image = np.ones((28, input_width))
+
+    result = helpers.zero_pad_centered_axis(input_image, 1, output_width)
+    assert result.shape[1] == output_width
+
+    # assert the we pad zeros on the left and on the right
+    # since image is all ones, we can check padding
+    # lets use contents bbox detector for checking
+    x0, y0, x1, y1 = helpers.calculate_binary_image_contents_bbox(result)
+    assert (x1 - x0)+1 == input_width
 

tests/test_number_generator.py
@@ -1,13 +1,44 @@
+import numpy as np
+from number_generator import core
 from number_generator import generate_numbers_sequence
+
+# configure the dataset to be local
+core._DIGITS_DATASET_FILEPATH = 'data/digits-dataset.pickle'
+
+def test_empty_sequence():
+
+    result = generate_numbers_sequence([], (5,10), 100)
+
+    # width must be correct even for empyt sequences
+    assert result.shape[1] == 100
+
+    # empty sequence must be all background
+    assert (result == 255).all()
 
 def test_output_size():
 
-    test_sequences = [
-        [1,2,3,4,5,6,1,2,3,4,5],
-        [1,2,4,5,6,2,3,4,5],
-        [0,2,4,5,6,1,2,3,4,5],
-        [0,,4,5,,1,2,3,9,1,1,4,5]
-    ]
+    # generate random sequences of different lengths and check if output width is correct
+    width = 210
+
+    num_sequences = 50
+
+    digits = [i for i in range(10)]
+
+    for i in range(num_sequences):
+
+        min_spacing = np.random.randint(1,5)
+        max_spacing = np.random.randint(5,15)
+
+        sequence_size = np.random.randint(1, 100)
+        sequence = np.random.choice(digits, size=sequence_size, replace=True)
+
+        result = generate_numbers_sequence(sequence, spacing_range=(min_spacing, max_spacing), image_width=width)
+
+        # all generated sequences must have precise correct output size
+        assert result.shape[1] == width
+
+        # resulting image cannot contain background only (black image)
+        assert not (result == 255).all()
 
 
 